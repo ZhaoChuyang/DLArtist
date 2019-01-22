4f4ad26c49af6b123b41c7b0b143b7cc7db4cce5
chuyang <zcy201420752@gmail.com>
2019-01-22 22:56:37 +0800

    editor template

--- resources/views/layouts/shop.blade.php
+++ resources/views/layouts/shop.blade.php
@@ -39,6 +39,9 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+
+        @yield('head')
+
     </head>
     <body>
       
@@ -215,5 +218,10 @@
         <script src="assets/js/imagesloaded.min.js"></script>
         <script src="assets/js/pace.min.js"></script>
         <script src="assets/js/main.js"></script>
+
+        @yield('script')
+
+        </div>
     </body>
+
 </html>
