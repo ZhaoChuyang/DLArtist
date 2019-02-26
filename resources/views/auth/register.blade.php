88bf1ed0d66aa8cee7e793c30130d93108effe11
chuyang <zcy201420752@gmail.com>
2019-02-26 23:03:04 +0800

    登录界面

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.edit')
 
 @section('content')
-<div class="container">
+<div class="container" style="margin-top: 100px;">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
